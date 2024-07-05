74e0a11ed327802b48c8a7764f4b73c2514e3de5
fcoeliesio <fcoeliesio@gmail.com>
2024-07-05 14:14:03 -0300

    update assets

--- lib/pages/splash/splash_page.dart
+++ lib/pages/splash/splash_page.dart
@@ -1,29 +1,50 @@
 import 'package:flutter/material.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
   const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  bool textoAnimated = false;
+
+  @override
+  void initState(){
+    super.initState();
+
+    Future.delayed(
+      Duration(microseconds: 300),
+      (){
+        setState(() {
+          textoAnimated = true;
+        });
+      }
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
       backgroundColor: Color.fromRGBO(86, 161, 226, 1),
-      body: SizedBox(
-        width: 1000,
-        height: 1000,
-        child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Text('texto 1'),
-          Container(
-            color: Color.fromARGB(255, 27, 92, 56),
-            height: 100,
-            width: 100,
-          ),
-          Text('Texto 2'),
-        ],
+      body: Stack( children: [
+      AnimatedPositioned(
+        duration: const Duration(seconds: 1),
+        top: textoAnimated ? 200: 50,
+        left: textoAnimated ? 0 : 0,
+      child: Image.asset('assets/logo.png')
       ),
-      ),
+      Positioned(
+        bottom: 200,
+        left: 12,
+        child: Text('Você é fraco, lhe falta brio!', style: TextStyle(
+          fontSize: 20, color: Colors.white,
+        ),
+        ),
+        ),
+    ],
+    ),
       );
   }
 }
